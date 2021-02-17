ba6bac2bfb688f99c092935ed169fba2d7187d61
James Tran <jatran@aggies.ncat.edu>
2021-02-17 13:31:47 -0500

    Slight modification to implementation %ASA-1-114001

--- id_pkg/src/log_parse.py
+++ id_pkg/src/log_parse.py
@@ -29,7 +29,7 @@ class LogParse:
 
         if id == 114001:
             # %ASA-1-114001: Failed to initialize 4GE SSM I/O card (error error_string)
-            x = re.search(r'error (\w+)', df.loc[id, 'Text'])
+            x = re.search(r' \(error (\w+)\)', df.loc[id, 'Text'])
             if x:
                 df.loc[id, 'Error'] = x.group(1)
 
