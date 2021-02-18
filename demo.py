466dd80ef90f3cfb452d18e3f76902ec0e620cbf
erewee3 <mnumelo@aggies.ncat.edu>
2021-02-17 21:59:07 -0500

    Implement %ASA-3-114019

--- demo.py
+++ demo.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 
 def show_aggie_pride():
-    df = pd.DataFrame(['Aggie Pride', 'Worldwide'])
+    # https://pandas.pydata.org/docs/user_guide/index.html
+    df = pd.DataFrame(['Aggie Pride', 'Worldwide', 'Aggies Do', 'Go Aggies', 'Aggies', 'GHOE!',
+                       'Achievement', 'We Graduating!!', 'A-G-G-I-E', 'NCAT',
+                       'Aggie Born, Aggie Bred, When I\'m gone I\'ll be Aggie Dead',
+                       'Greatest Homecoming On Earth', 'Mens et Manus (Mind and Hand)',
+                       'yay aggies','AGGIES CODE!', 'Aggie Land Forever'])
     print(df)
 
 
