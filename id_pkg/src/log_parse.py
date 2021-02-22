789f9718a5b76d3e5cb6943d34a80661eee0fee8
austin3dprinting <austin@austingeorge.com>
2021-02-21 20:21:19 -0500

    Added %ASA-3-114018: Failed to set port speed in 4GE SSM I/O card (error error_string).

--- id_pkg/src/log_parse.py
+++ id_pkg/src/log_parse.py
@@ -57,6 +57,13 @@ class LogParse:
             if match:
                 df.loc[id, 'Error'] = match.group(1)
 
+        if id == 114018:
+            # %ASA-3-114018: Failed to set port speed in 4GE SSM I/O card (error error_string).
+            m = re.search(r'\(error (\w+)', df.loc[id, 'Text'])
+            if m:
+                df.loc[id, 'Error'] = m.group(1)
+
+
         return df
 
     def parse_syslog_file(self, syslog_file):
