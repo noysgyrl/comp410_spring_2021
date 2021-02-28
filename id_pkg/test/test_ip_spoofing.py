827ffe4e6f8c0305e00fb9f8db523c1e8b200b32
claesmk <claesmk@gmail.com>
2021-02-28 06:05:50 -0500

    Added test_ip_spoofing stub

---
+++ id_pkg/test/test_ip_spoofing.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestIpSpoofing(unittest.TestCase):
+    def test_ip_spoofing_stub(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
