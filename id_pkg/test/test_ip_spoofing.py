58e19ac82f0f80d70934b3710b9c9d4b2e28c558
claesmk <claesmk@gmail.com>
2021-02-28 17:41:36 -0500

    Restructure due to test case ordering

--- id_pkg/test/test_ip_spoofing.py
+++ id_pkg/test/test_ip_spoofing.py
@@ -9,31 +9,33 @@ class TestIpSpoofing(unittest.TestCase):
     git_root = os.path.join(git.Repo('.', search_parent_directories=True).working_tree_dir, 'id_pkg')
     syslog_file = os.path.join(git_root, 'data', 'ip_spoofing.txt')
 
+    # Rather than get into ordering test cases work around the problem by making sure
+    # the test log file gets generated whenever this class is created.
+
+    # %ASA-2-106016: Deny IP spoof from (IP_address) to IP_address on interface interface_name.
+    # Sep 12 2014 06:50:53 HOST : %ASA-2-106016: Deny IP spoof from (10.1.1.1) to 10.11.11.11 on
+    #   interface TestInterface
+    info = {'Date': 'Sep 12 2014 06:50:53',
+            'Host': 'HOST',
+            'ID': '%ASA-2-106016',
+            'Interface': 'TestInterface'}
+
+    # Create a sample log file
+    # https://docs.python.org/3/tutorial/inputoutput.html
+    with open(syslog_file, 'w') as f:
+        for ip_address_d in range(1,256,1):
+            # Create the first part of the message
+            log_string = info['Date'] + ' ' + info['Host'] + ' : ' + info['ID'] + ': '
+            # Next add the source IP address message
+            log_string = log_string + 'Deny IP spoof from (10.1.1.1) to '
+            # Now add the destination IP
+            log_string = log_string + '10.11.11.' + str(ip_address_d)
+            # Terminate the message with the interface name
+            log_string = log_string + ' on interface ' + info['Interface'] + '\n'
+            f.write(log_string)
+
     def test_ip_spoofing_stub(self):
         self.assertEqual(True, True)
-
-    def test_ip_spoofing_create_sample_log(self):
-        # %ASA-2-106016: Deny IP spoof from (IP_address) to IP_address on interface interface_name.
-        # Sep 12 2014 06:50:53 HOST : %ASA-2-106016: Deny IP spoof from (10.1.1.1) to 10.11.11.11 on
-        #   interface TestInterface
-        info = {'Date': 'Sep 12 2014 06:50:53',
-                'Host': 'HOST',
-                'ID': '%ASA-2-106016',
-                'Interface': 'TestInterface'}
-
-        # Create a sample log file
-        # https://docs.python.org/3/tutorial/inputoutput.html
-        with open(self.syslog_file, 'w') as f:
-            for ip_address_d in range(1,256,1):
-                # Create the first part of the message
-                log_string = info['Date'] + ' ' + info['Host'] + ' : ' + info['ID'] + ': '
-                # Next add the source IP address message
-                log_string = log_string + 'Deny IP spoof from (10.1.1.1) to '
-                # Now add the destination IP
-                log_string = log_string + '10.11.11.' + str(ip_address_d)
-                # Terminate the message with the interface name
-                log_string = log_string + ' on interface ' + info['Interface'] + '\n'
-                f.write(log_string)
 
     def test_ip_spoofing_parse_log(self):
         # Create an IdParse object
