3b32d427c724ae8465b8803dc2f08b8ad9bee035
erewee3 <mnumelo@aggies.ncat.edu>
2021-03-04 11:24:14 -0500

    Requirement now working

---
+++ id_pkg/test/test_acl_drop.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestACLDROP(unittest.TestCase):
+    def test_acl_drop_stub(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
