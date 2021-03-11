a4709e7a520d51cde987d57ded42362a8e9a001e
James Tran <jatran@aggies.ncat.edu>
2021-03-10 19:24:44 -0500

    add new test case for firewall check

---
+++ id_pkg/test/test_firewall.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestFirewall(unittest.TestCase):
+    def test_firewall_stub(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
