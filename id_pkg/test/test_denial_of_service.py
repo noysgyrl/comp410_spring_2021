4db9f1d4e28ab7af63070eb46a68051024170423
Malcolm R. Jones <maljonesdev@gmail.com>
2021-03-14 14:13:37 -0400

    Create unit test for Denial Of Service

---
+++ id_pkg/test/test_denial_of_service.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestDenialOfService(unittest.TestCase):
+    def test_denial_of_service_stub(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
