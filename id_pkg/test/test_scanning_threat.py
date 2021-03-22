1b4c81827589cca30c39e5d8c290fcf8ebd4f482
lndavis6 <lancendavis47@gmail.com>
2021-03-22 18:59:49 -0400

    Created unit test for Scanning Threat

--- id_pkg/test/test_scanning_threat.py
+++ id_pkg/test/test_scanning_threat.py
@@ -5,3 +5,8 @@ import os
 import pandas as pd
 
 class TestScanningThreat(unittest.TestCase):
+    def test_scanning_threat_stub(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
