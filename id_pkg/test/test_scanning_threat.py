1fc0e2a4a8f9fea3726c8a6a86cd66e65e35c5c4
lndavis6 <lancendavis47@gmail.com>
2021-03-23 18:22:34 -0400

    Committing fixed test code

--- id_pkg/test/test_scanning_threat.py
+++ id_pkg/test/test_scanning_threat.py
@@ -5,39 +5,38 @@ import id_pkg as intrusion_detect
 # import pandas as pd
 
 class TestScanningThreat(unittest.TestCase):
+    # Get the path to the data directory in the git repo
+    git_root = os.path.join(git.Repo('.', search_parent_directories=True).working_tree_dir, 'id_pkg')
+    syslog_file = os.path.join(git_root, 'data', 'scanning_threat.txt')
+
+    # % ASA - 4 - 733101: Object objectIP ( is targeted | is attacking). Current burst rate is rate_val per second,
+    # max configured rate is rate_val; Current average rate is rate_val per second, max configured rate is rate_val;
+    # Cumulative total count is total_cnt.
+
+    info = {'Date': 'March 30 2021 03:30:30',
+            'Host': 'TEAMNULL',
+            'Type': 'targeted',
+            'ID': '%ASA-4-733101',
+            'rate_val': '20',
+            'total_cnt': '100'}
+
+    # Create a sample log file
+    with open(syslog_file, 'w') as f:
+        for ip_address_d in range(1, 256, 1):
+            # Create first part of the message
+            log_string = info['Date'] + ' ' + info['ID'] + ': ' + info['Host'] + ' 10.11.11.' + str(ip_address_d)
+            # Next add whether the port is attacking or being targeted
+            log_string = log_string + ' is ' + info['Type']
+            # Next add all of the burst rates
+            log_string = log_string + 'Current burst rate is ' + info['rate_val'] + ' per second, '
+            log_string = log_string + 'max configured rate is ' + info['rate_val'] + '; '
+            log_string = log_string + ', Current average rate is ' + info['rate_val'] + ' per second, '
+            log_string = log_string + 'max configured rate is ' + info['rate_val'] + '; '
+            log_string = log_string + 'Cumulative total count is ' + info['total_cnt'] + '\n'
+            f.write(log_string)
+
     def test_scanning_threat_stub(self):
         self.assertEqual(True, True)
-
-    def test_scanning_threat_create_sample_log(self):
-        # % ASA - 4 - 733101: Object objectIP ( is targeted | is attacking). Current burst rate is rate_val per second,
-        # max configured rate is rate_val; Current average rate is rate_val per second, max configured rate is rate_val;
-        # Cumulative total count is total_cnt.
-
-        info = {'Date': 'March 30 2021 03:30:30',
-                'Host': 'TEAMNULL',
-                'Type': 'targeted',
-                'ID': '%ASA-4-733101',
-                'rate_val': '20',
-                'total_cnt': '100'}
-
-        # Get the path to the data directory in the git repo
-        git_root = os.path.join(git.Repo('.', search_parent_directories=True).working_tree_dir, 'id_pkg')
-        data_path = os.path.join(git_root, 'data')
-
-        # Create a sample log file
-        with open(os.path.join(data_path, 'scanning_threat.txt'), 'w') as f:
-            for ip_address_d in range(1, 256, 1):
-                # Create first part of the message
-                log_string = info['Date'] + ' ' + info['ID'] + ': ' + info['Host'] + ' 10.11.11.' + str(ip_address_d)
-                # Next add whether the port is attacking or being targeted
-                log_string = log_string + ' is ' + info['Type']
-                # Next add all of the burst rates
-                log_string = log_string + 'Current burst rate is ' + info['rate_val'] + ' per second, '
-                log_string = log_string + 'max configured rate is ' + info['rate_val'] + '; '
-                log_string = log_string + 'Current average rate is ' + info['rate_val'] + ' per second, '
-                log_string = log_string + 'max configured rate is ' + info['rate_val'] + '; '
-                log_string = log_string + 'Cumulative total count is ' + info['total_cnt'] + '\n'
-                f.write(log_string)
 
     def test_scanning_threat_parse_log(self):
         id_syslog = intrusion_detect.IdParse(self.syslog_file)
