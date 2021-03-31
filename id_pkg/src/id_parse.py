494559b15569f84b0de5dd61b0a9de82bf4100fb
MKC <claesmk@gmail.com>
2021-03-30 20:33:29 -0400

    parse log in syn attack

--- id_pkg/src/id_parse.py
+++ id_pkg/src/id_parse.py
@@ -32,6 +32,9 @@ class IdParse(LogParse):
     def has_denied_icmp(self):
         return (self.df['ID'] == 313004).any()
 
+    def has_syn_attack(self):
+        return (self.df['ID'] == 419002).any()
+
     def handle_asa_message(self, rec):
         """Implement ASA specific messages"""
         # %ASA-2-106016: Deny IP spoof from (10.1.1.1) to 10.11.11.19 on interface TestInterface
@@ -108,6 +111,16 @@ class IdParse(LogParse):
                 rec['Source'] = m.group(2)
                 rec['Interface'] = m.group(3)
                 rec['Destination'] = m.group(4)
+
+        # %ASA-4-419002: Received duplicate TCP SYN from in_interface:src_address/src_port to
+        # out_interface:dest_address/dest_port
+        if rec['ID'] == 419002:
+            m = re.search(r'Received duplicate TCP SYN from in_interface:(\d+\.\d+\.\d+\.\d+)/(\d+) to out_interface:(\d+\.\d+\.\d+\.\d+)/(\d+)', rec['Text'])
+            if m:
+                rec['Source'] = m.group(1)
+                rec['Source Port'] = m.group(2)
+                rec['Destination'] = m.group(3)
+                rec['Destination Port'] = m.group(4)
 
         return rec
 
